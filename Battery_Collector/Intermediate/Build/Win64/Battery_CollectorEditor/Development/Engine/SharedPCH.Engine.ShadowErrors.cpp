dc965d6a699835b1bc7240e35d72f4b0865a6ffa
MuMuKim <kda5772@naver.com>
2021-06-18 21:39:18 +0900

    210618 BatteryCollector Settings
    에픽 게임스 런처 - 학습 - 콘텐츠 예제 프로젝트를 실행한다 - 새로운 프로젝트를 C++의 3인칭 모드로 만들고, 콘텐츠 예제의 에셋3개를 새로 만든 프로젝트에 이주시킨다 - BP_DemoRoom, SM_Battery, P_Electricity 에셋을 이주하고, 맵셋팅을 한다.

---
+++ Battery_Collector/Intermediate/Build/Win64/Battery_CollectorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29914
+#include "C:/Users/JIHOONKIM/Documents/unity/Unreal_BatteryCollector/Battery_Collector/Intermediate/Build/Win64/Battery_CollectorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
